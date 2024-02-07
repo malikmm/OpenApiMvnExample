7111e4476c7e8de2657e4c9e93b0c582406d718d
malikmm <malikmm@hotmail.com>
2024-02-07 19:21:56 +0000

    inital setup

---
+++ src/main/java/com/malik/springstartermvn/SpringstartermvnApplication.java
@@ -0,0 +1,13 @@
+package com.malik.springstartermvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstartermvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstartermvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malik/springstartermvn/SpringstartermvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malik.springstartermvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstartermvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
